1cb1c949250901b66d7007f05a10c7d73a120f3a
arytgra <88172851+arytgra@users.noreply.github.com>
2022-03-10 07:50:31 +0700

    Add files via upload

---
+++ Alert.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';  
+class Alert extends Component {  
+  render(){  
+    return (  
+      <div className="alert alert-success">  
+        <h4>Info</h4>  
+        Belajar React JS itu menyenangkan  
+      </div>  
+    );  
+  }  
+}  
+
+
---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Alert from './Components/Alert';   
+
+function App() {
+  return (
+    <div className="alert alert-success">  
+        <h4>Info</h4>  
+        Belajar React JS itu menyenangkan  
+      </div>  
+
+  );
+}
+
+export default App;
